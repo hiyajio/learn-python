d0bb746a1ea00c33c50b0d78617a160ece2b99f8
Juan Sergio Buenviaje <jbuenvia@nd.edu>
2019-10-02 15:52:08 -0400

    Added files via upload

---
+++ speak_func.py
@@ -0,0 +1,11 @@
+# Define speak below:
+def speak(animal="dog"):
+    if animal == "dog":
+        return "woof"
+    elif animal == "pig":
+        return "oink"
+    elif animal == "duck":
+        return "quack"
+    elif animal == "cat":
+        return "meow"
+    return "?"
