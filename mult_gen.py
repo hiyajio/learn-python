e187f6f09565b2334253044bcddd844879cd47c4
Juan Sergio Buenviaje <jbuenvia@nd.edu>
2019-10-07 10:53:57 -0400

    Add files via upload

---
+++ mult_gen.py
@@ -0,0 +1,17 @@
+'''
+evens = get_multiples(2, 3)
+next(evens) # 2
+next(evens) # 4
+next(evens) # 6
+next(evens) # StopIteration
+
+default_multiples = get_multiples()
+list(default_multiples) # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+'''
+
+def get_multiples(num = 1, count = 10):
+    next_num = num
+    while count > 0:
+        yield next_num
+        count -= 1
+        next_num += num
---
+++ song_gen.py
@@ -0,0 +1,20 @@
+'''
+kombucha_song = make_song(5, "kombucha")
+next(kombucha_song) # '5 bottles of kombucha on the wall.'
+next(kombucha_song) # '4 bottles of kombucha on the wall.'
+next(kombucha_song) # '3 bottles of kombucha on the wall.'
+next(kombucha_song) # '2 bottles of kombucha on the wall.'
+next(kombucha_song) # 'Only 1 bottle of kombucha left!'
+next(kombucha_song) # 'No more kombucha!'
+next(kombucha_song) # StopIteration
+
+default_song = make_song()
+next(default_song) # '99 bottles of soda on the wall.'
+'''
+
+def make_song(i=99, beverage="soda"):
+    while i > 1:
+        yield str(i) + " bottles of " + beverage + " on the wall."
+        i -= 1
+    yield "Only 1 bottle of " + beverage + " left!"
+    yield "No more " + beverage + "!"
