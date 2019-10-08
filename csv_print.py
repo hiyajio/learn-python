e9c4bcc50686960561824eb1f0c336962e223807
Juan Sergio Buenviaje <jbuenvia@nd.edu>
2019-10-08 11:04:09 -0400

    Add files via upload

---
+++ csv_print.py
@@ -0,0 +1,13 @@
+'''
+print_users() # None
+# prints to the console:
+# Colt Steele
+'''
+
+import csv
+
+def print_users():
+    with open("users.csv") as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        for row in csv_reader:
+            print("{} {}".format(row['First Name'], row['Last Name']))
